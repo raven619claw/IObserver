b26e15b2408505024a6d9747feb42c7e1ca4f62b
Ravi Yadav <ravi.yadav@proptiger.com>
2018-01-16 18:54:58 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,39 @@
+import 'intersectionObserverService';
+// call this func to setup the service and use for elements
+function _setUpIntersectionObForModules() {
+    let options = {
+      //set this attr on the elements to be used as entry for Observer
+      //required is only the dataAttr
+      //rest are optional
+        dataAttr: 'data-io-type'
+    }
+    let ob = new IObService('module-loader', options);
+    if (!ob.featurePresent) {
+    //use the default polyfill you would use in the project
+        callback_1();
+        callback_2();
+        return;
+    }
+    let modulesToLoad = [SELECTORS.EL_1, SELECTORS.EL_2];
+
+    let callbackfnObj = {
+    //define the type as the attr property of data-io-type on the el
+        'el_type_1': {
+            type: 'el_type_1',
+            limit: 0,
+            once: true,
+            fn: (args) => {
+                callback_1();
+            }
+        },
+        'el_type_2': {
+            type: 'el_type_2',
+            limit: 0,
+            once: true,
+            fn: (args) => {
+                callback_2;
+            }
+        }
+    };
+    ob.addTargets(modulesToLoad, callbackfnObj);
+};
